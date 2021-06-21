7cfe91f71891771240b8dc2288c861efb0869138
Stephan Gerhard <git@unidesign.ch>
2021-06-21 15:12:10 +0200

    Adding some more header informations as test

--- apps/example-app/src/pages/index.js
+++ apps/example-app/src/pages/index.js
@@ -10,9 +10,15 @@ const IndexPage = () => {
   const callFunction = () => {
     axios({
       method: 'get',
-      crossDomain: true,
-      url: 'https://wclee.api.zetta.ai/info/api/v2/datastacks',
+      // crossDomain: false,
+      allowedHeaders: ["Content-Type", "Authorization", "Access-Control-Allow-Methods", "Access-Control-Request-Headers"],
+      credentials: true,
+      enablePreflight: true,
+      withCredentials: false,
+      url: 'https://wclee.api.zetta.ai/wclee/info/api/v2/datastacks',
       headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
         'Content-Type': 'application/json',
         'Authorization': "Bearer " + config["FANC_AUTH_TOKEN"]
       }
